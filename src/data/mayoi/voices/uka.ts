f5a106b379794cf3b680c6df20877fde72dbca26
sua <u2yuni.info@gmail.com>
2026-06-30 21:05:41 +0900

    うかちゃんサンプル動画設定

--- src/data/mayoi/voices/uka.ts
+++ src/data/mayoi/voices/uka.ts
@@ -7,7 +7,7 @@ export const UKA_VOICES: VoiceLibrary[] = [
     version: '1.0',
     description: '落ち着いた女性声。',
     downloadUrl: 'https://bowlroll.net/file/355785',
-    sampleYoutubeUrl: '',
+    sampleNiconicoUrl: 'https://www.nicovideo.jp/watch/sm46493598',
     releaseDate: '2026-06-30',
     iconUrl: 'assets/images/mayoi/uka_normal/uka_icon3.png', // Default icon
     tags: ['連続音'],
